b66651b0f58731465c5cc8e8ecc7f4a858b8cadd
Yuliya Savickaya <ysavickaya@marfa-tech.com>
2025-06-17 15:58:28 +0300

    add headless

--- conftest.py
+++ conftest.py
@@ -6,8 +6,8 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='function')
 def browser():
     options = Options()
-   #options.add_argument('--headless')
-    options.add_argument('--window-size=1280,760')
+    options.add_argument('--headless')
+    #options.add_argument('--window-size=1280,760')
 
     driver = webdriver.Chrome(options=options)
 
