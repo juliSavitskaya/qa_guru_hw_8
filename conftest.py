379ff47f0f86a11470e16579c53977251141d141
Yuliya Savickaya <ysavickaya@marfa-tech.com>
2025-06-17 16:07:13 +0300

    add arguments

--- conftest.py
+++ conftest.py
@@ -7,6 +7,9 @@ from selenium.webdriver.chrome.options import Options
 def browser():
     options = Options()
     options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     #options.add_argument('--window-size=1280,760')
 
     driver = webdriver.Chrome(options=options)
