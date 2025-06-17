63c53860bfb34576a8c31d28aa5f08f98acd4ee8
Yuliya Savickaya <ysavickaya@marfa-tech.com>
2025-06-17 18:45:25 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -6,11 +6,11 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='function')
 def browser():
     options = Options()
-    options.add_argument('--headless')
+    #options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    #options.add_argument('--window-size=1280,760')
+    options.add_argument('--window-size=1280,760')
 
     driver = webdriver.Chrome(options=options)
 
