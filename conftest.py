6b3e19218ca75410b251551d9beed80addf6d1cd
Yuliya Savickaya <ysavickaya@marfa-tech.com>
2025-06-17 19:27:46 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope='function')
@@ -10,9 +12,12 @@ def browser():
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    #options.add_argument('--window-size=1280,760')
+    # options.add_argument('--window-size=1280,760')
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
 
     yield driver
 
